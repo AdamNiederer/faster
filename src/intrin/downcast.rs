7543596022ceb35d8543930fd37620caab5c9a9f
Adam Niederer <adam.niederer@gmail.com>
2017-11-18 00:46:18 -0500

    Add casts, upcasts, and downcasts
    Casts are pretty much done, but downcasts and upcasts are probably either broken
    or incomplete

---
+++ src/intrin/downcast.rs
@@ -0,0 +1,257 @@
+use stdsimd::simd::*;
+use stdsimd::vendor::*;
+use intrin::transmute::*;
+
+pub trait Downcast<T> {
+    fn saturating_downcast(self, other: Self) -> T;
+}
+
+impl Downcast<i16x8> for i32x4 {
+    #[inline(always)]
+    #[cfg(target_feature = "sse2")]
+    fn saturating_downcast(self, other: Self) -> i16x8 {
+        unsafe { _mm_packs_epi32(self, other) }
+    }
+
+    #[inline(always)]
+    #[cfg(not(target_feature = "sse2"))]
+    fn saturating_downcast(self, other: Self) -> i16x8 {
+        i16x8::new(self.extract(0).min(0x00007FFF).max(-0x00008000) as i16,
+                   self.extract(1).min(0x00007FFF).max(-0x00008000) as i16,
+                   self.extract(2).min(0x00007FFF).max(-0x00008000) as i16,
+                   self.extract(3).min(0x00007FFF).max(-0x00008000) as i16,
+                   other.extract(0).min(0x00007FFF).max(-0x00008000) as i16,
+                   other.extract(1).min(0x00007FFF).max(-0x00008000) as i16,
+                   other.extract(2).min(0x00007FFF).max(-0x00008000) as i16,
+                   other.extract(3).min(0x00007FFF).max(-0x00008000) as i16)
+    }
+}
+
+impl Downcast<i8x16> for i16x8 {
+    #[inline(always)]
+    #[cfg(target_feature = "sse2")]
+    fn saturating_downcast(self, other: Self) -> i8x16 {
+        unsafe { _mm_packs_epi16(self, other) }
+    }
+
+    #[inline(always)]
+    #[cfg(not(target_feature = "sse2"))]
+    fn saturating_downcast(self, other: Self) -> i16x8 {
+        i8x16::new(self.extract(0).min(0x007F).max(-0x0080) as i8,
+                   self.extract(1).min(0x007F).max(-0x0080) as i8,
+                   self.extract(2).min(0x007F).max(-0x0080) as i8,
+                   self.extract(3).min(0x007F).max(-0x0080) as i8,
+                   self.extract(4).min(0x007F).max(-0x0080) as i8,
+                   self.extract(5).min(0x007F).max(-0x0080) as i8,
+                   self.extract(6).min(0x007F).max(-0x0080) as i8,
+                   self.extract(7).min(0x007F).max(-0x0080) as i8,
+                   other.extract(0).min(0x007F).max(-0x0080) as i8,
+                   other.extract(1).min(0x007F).max(-0x0080) as i8,
+                   other.extract(2).min(0x007F).max(-0x0080) as i8,
+                   other.extract(3).min(0x007F).max(-0x0080) as i8,
+                   other.extract(4).min(0x007F).max(-0x0080) as i8,
+                   other.extract(5).min(0x007F).max(-0x0080) as i8,
+                   other.extract(6).min(0x007F).max(-0x0080) as i8,
+                   other.extract(7).min(0x007F).max(-0x0080) as i8)
+    }
+}
+
+impl Downcast<u16x8> for u32x4 {
+    // Blocked by stdsimd
+    // #[inline(always)]
+    // #[cfg(target_feature = "sse4.1")]
+    // fn saturating_downcast(self, other: Self) -> u16x8 {
+    //     unsafe { _mm_packus_epi32(self, other) }
+    // }
+
+    #[inline(always)]
+    // #[cfg(not(target_feature = "sse4.1"))]
+    fn saturating_downcast(self, other: Self) -> u16x8 {
+        u16x8::new(self.extract(0).min(0x0000FFFF) as u16,
+                   self.extract(1).min(0x0000FFFF) as u16,
+                   self.extract(2).min(0x0000FFFF) as u16,
+                   self.extract(3).min(0x0000FFFF) as u16,
+                   other.extract(0).min(0x0000FFFF) as u16,
+                   other.extract(1).min(0x0000FFFF) as u16,
+                   other.extract(2).min(0x0000FFFF) as u16,
+                   other.extract(3).min(0x0000FFFF) as u16)
+    }
+}
+
+impl Downcast<u8x16> for u16x8 {
+    #[inline(always)]
+    #[cfg(target_feature = "sse2")]
+    fn saturating_downcast(self, other: Self) -> u8x16 {
+        unsafe { _mm_packus_epi16(self.be_i16s(), other.be_i16s()) }
+    }
+
+    #[inline(always)]
+    #[cfg(not(target_feature = "sse2"))]
+    fn saturating_downcast(self, other: Self) -> u16x8 {
+        u8x16::new(self.extract(0).min(0x007F).max(-0x0080) as u8,
+                   self.extract(1).min(0x007F).max(-0x0080) as u8,
+                   self.extract(2).min(0x007F).max(-0x0080) as u8,
+                   self.extract(3).min(0x007F).max(-0x0080) as u8,
+                   self.extract(4).min(0x007F).max(-0x0080) as u8,
+                   self.extract(5).min(0x007F).max(-0x0080) as u8,
+                   self.extract(6).min(0x007F).max(-0x0080) as u8,
+                   self.extract(7).min(0x007F).max(-0x0080) as u8,
+                   other.extract(0).min(0x007F).max(-0x0080) as u8,
+                   other.extract(1).min(0x007F).max(-0x0080) as u8,
+                   other.extract(2).min(0x007F).max(-0x0080) as u8,
+                   other.extract(3).min(0x007F).max(-0x0080) as u8,
+                   other.extract(4).min(0x007F).max(-0x0080) as u8,
+                   other.extract(5).min(0x007F).max(-0x0080) as u8,
+                   other.extract(6).min(0x007F).max(-0x0080) as u8,
+                   other.extract(7).min(0x007F).max(-0x0080) as u8)
+    }
+}
+
+impl Downcast<i16x16> for i32x8 {
+    #[inline(always)]
+    #[cfg(target_feature = "sse2")]
+    fn saturating_downcast(self, other: Self) -> i16x16 {
+        unsafe { _mm256_packs_epi32(self, other) }
+    }
+
+    #[inline(always)]
+    #[cfg(not(target_feature = "sse2"))]
+    fn saturating_downcast(self, other: Self) -> i16x16 {
+        i16x16::new(self.extract(0).min(0x00007FFF).max(-0x00008000) as i16,
+                    self.extract(1).min(0x00007FFF).max(-0x00008000) as i16,
+                    self.extract(2).min(0x00007FFF).max(-0x00008000) as i16,
+                    self.extract(3).min(0x00007FFF).max(-0x00008000) as i16,
+                    self.extract(4).min(0x00007FFF).max(-0x00008000) as i16,
+                    self.extract(5).min(0x00007FFF).max(-0x00008000) as i16,
+                    self.extract(6).min(0x00007FFF).max(-0x00008000) as i16,
+                    self.extract(7).min(0x00007FFF).max(-0x00008000) as i16,
+                    other.extract(0).min(0x00007FFF).max(-0x00008000) as i16,
+                    other.extract(1).min(0x00007FFF).max(-0x00008000) as i16,
+                    other.extract(2).min(0x00007FFF).max(-0x00008000) as i16,
+                    other.extract(3).min(0x00007FFF).max(-0x00008000) as i16,
+                    other.extract(4).min(0x00007FFF).max(-0x00008000) as i16,
+                    other.extract(5).min(0x00007FFF).max(-0x00008000) as i16,
+                    other.extract(6).min(0x00007FFF).max(-0x00008000) as i16,
+                    other.extract(7).min(0x00007FFF).max(-0x00008000) as i16)
+    }
+}
+
+impl Downcast<i8x32> for i16x16 {
+    #[inline(always)]
+    #[cfg(target_feature = "sse2")]
+    fn saturating_downcast(self, other: Self) -> i8x32 {
+        unsafe { _mm256_packs_epi16(self, other) }
+    }
+
+    #[inline(always)]
+    #[cfg(not(target_feature = "sse2"))]
+    fn saturating_downcast(self, other: Self) -> i16x16 {
+        i8x32::new(self.extract(0).min(0x007F).max(-0x0080) as u8,
+                   self.extract(1).min(0x007F).max(-0x0080) as u8,
+                   self.extract(2).min(0x007F).max(-0x0080) as u8,
+                   self.extract(3).min(0x007F).max(-0x0080) as u8,
+                   self.extract(4).min(0x007F).max(-0x0080) as u8,
+                   self.extract(5).min(0x007F).max(-0x0080) as u8,
+                   self.extract(6).min(0x007F).max(-0x0080) as u8,
+                   self.extract(7).min(0x007F).max(-0x0080) as u8,
+                   self.extract(8).min(0x007F).max(-0x0080) as u8,
+                   self.extract(9).min(0x007F).max(-0x0080) as u8,
+                   self.extract(10).min(0x007F).max(-0x0080) as u8,
+                   self.extract(11).min(0x007F).max(-0x0080) as u8,
+                   self.extract(12).min(0x007F).max(-0x0080) as u8,
+                   self.extract(13).min(0x007F).max(-0x0080) as u8,
+                   self.extract(14).min(0x007F).max(-0x0080) as u8,
+                   self.extract(15).min(0x007F).max(-0x0080) as u8,
+                   other.extract(0).min(0x007F).max(-0x0080) as u8,
+                   other.extract(1).min(0x007F).max(-0x0080) as u8,
+                   other.extract(2).min(0x007F).max(-0x0080) as u8,
+                   other.extract(3).min(0x007F).max(-0x0080) as u8,
+                   other.extract(4).min(0x007F).max(-0x0080) as u8,
+                   other.extract(5).min(0x007F).max(-0x0080) as u8,
+                   other.extract(6).min(0x007F).max(-0x0080) as u8,
+                   other.extract(7).min(0x007F).max(-0x0080) as u8,
+                   other.extract(8).min(0x007F).max(-0x0080) as u8,
+                   other.extract(9).min(0x007F).max(-0x0080) as u8,
+                   other.extract(10).min(0x007F).max(-0x0080) as u8,
+                   other.extract(11).min(0x007F).max(-0x0080) as u8,
+                   other.extract(12).min(0x007F).max(-0x0080) as u8,
+                   other.extract(13).min(0x007F).max(-0x0080) as u8,
+                   other.extract(14).min(0x007F).max(-0x0080) as u8,
+                   other.extract(15).min(0x007F).max(-0x0080) as u8)
+    }
+}
+
+impl Downcast<u16x16> for u32x8 {
+    // Blocked by stdsimd
+    // #[inline(always)]
+    // #[cfg(target_feature = "sse4.1")]
+    // fn saturating_downcast(self, other: Self) -> u16x16 {
+    //     unsafe { _mm256_packus_epi32(self, other) }
+    // }
+
+    #[inline(always)]
+    // #[cfg(not(target_feature = "sse4.1"))]
+    fn saturating_downcast(self, other: Self) -> u16x16 {
+        u16x16::new(self.extract(0).min(0x0000FFFF) as u16,
+                    self.extract(1).min(0x0000FFFF) as u16,
+                    self.extract(2).min(0x0000FFFF) as u16,
+                    self.extract(3).min(0x0000FFFF) as u16,
+                    self.extract(4).min(0x0000FFFF) as u16,
+                    self.extract(5).min(0x0000FFFF) as u16,
+                    self.extract(6).min(0x0000FFFF) as u16,
+                    self.extract(7).min(0x0000FFFF) as u16,
+                    other.extract(0).min(0x0000FFFF) as u16,
+                    other.extract(1).min(0x0000FFFF) as u16,
+                    other.extract(2).min(0x0000FFFF) as u16,
+                    other.extract(3).min(0x0000FFFF) as u16,
+                    other.extract(4).min(0x0000FFFF) as u16,
+                    other.extract(5).min(0x0000FFFF) as u16,
+                    other.extract(6).min(0x0000FFFF) as u16,
+                    other.extract(7).min(0x0000FFFF) as u16)
+    }
+}
+
+impl Downcast<u8x32> for u16x16 {
+    #[inline(always)]
+    #[cfg(target_feature = "sse2")]
+    fn saturating_downcast(self, other: Self) -> u8x32 {
+        unsafe { _mm256_packus_epi16(self.be_i16s(), other.be_i16s()) }
+    }
+
+    #[inline(always)]
+    #[cfg(not(target_feature = "sse2"))]
+    fn saturating_downcast(self, other: Self) -> u16x16 {
+        u8x32::new(self.extract(0).min(0x00FF) as u8,
+                   self.extract(1).min(0x00FF) as u8,
+                   self.extract(2).min(0x00FF) as u8,
+                   self.extract(3).min(0x00FF) as u8,
+                   self.extract(4).min(0x00FF) as u8,
+                   self.extract(5).min(0x00FF) as u8,
+                   self.extract(6).min(0x00FF) as u8,
+                   self.extract(7).min(0x00FF) as u8,
+                   self.extract(8).min(0x00FF) as u8,
+                   self.extract(9).min(0x00FF) as u8,
+                   self.extract(10).min(0x00FF) as u8,
+                   self.extract(11).min(0x00FF) as u8,
+                   self.extract(12).min(0x00FF) as u8,
+                   self.extract(13).min(0x00FF) as u8,
+                   self.extract(14).min(0x00FF) as u8,
+                   self.extract(15).min(0x00FF) as u8,
+                   other.extract(0).min(0x00FF) as u8,
+                   other.extract(1).min(0x00FF) as u8,
+                   other.extract(2).min(0x00FF) as u8,
+                   other.extract(3).min(0x00FF) as u8,
+                   other.extract(4).min(0x00FF) as u8,
+                   other.extract(5).min(0x00FF) as u8,
+                   other.extract(6).min(0x00FF) as u8,
+                   other.extract(7).min(0x00FF) as u8,
+                   other.extract(8).min(0x00FF) as u8,
+                   other.extract(9).min(0x00FF) as u8,
+                   other.extract(10).min(0x00FF) as u8,
+                   other.extract(11).min(0x00FF) as u8,
+                   other.extract(12).min(0x00FF) as u8,
+                   other.extract(13).min(0x00FF) as u8,
+                   other.extract(14).min(0x00FF) as u8,
+                   other.extract(15).min(0x00FF) as u8)
+    }
+}
---
+++ src/intrin/upcast.rs
@@ -0,0 +1,259 @@
+use stdsimd::simd::*;
+use stdsimd::vendor::*;
+use intrin::transmute::*;
+
+pub trait Upcast<T> {
+    fn upcast(self) -> (T, T);
+}
+
+impl Upcast<f64x2> for f32x4 {
+    #[inline(always)]
+    #[cfg(target_feature = "sse2")]
+    fn upcast(self) -> (f64x2, f64x2) {
+        // Shuffle the vector as i32s for better perf
+        unsafe { (_mm_cvtps_pd(self), _mm_cvtps_pd(_mm_shuffle_epi32(self.be_i32s(), 0x0E).be_f32s_unchecked())) }
+    }
+
+    #[inline(always)]
+    #[cfg(not(target_feature = "sse2"))]
+    fn upcast(self) -> (f64x2, f64x2) {
+        (f64x2::new(self.extract(0) as f64,
+                    self.extract(1) as f64),
+         f64x2::new(self.extract(2) as f64,
+                    self.extract(3) as f64))
+    }
+}
+
+impl Upcast<f64x2> for i32x4 {
+    #[inline(always)]
+    #[cfg(target_feature = "sse2")]
+    fn upcast(self) -> (f64x2, f64x2) {
+        unsafe { (_mm_cvtepi32_pd(self), _mm_cvtepi32_pd(_mm_shuffle_epi32(self, 0x0E))) }
+    }
+
+    #[inline(always)]
+    #[cfg(not(target_feature = "sse2"))]
+    fn upcast(self) -> (f64x2, f64x2) {
+        (f64x2::new(self.extract(0) as f64,
+                    self.extract(1) as f64),
+         f64x2::new(self.extract(2) as f64,
+                    self.extract(3) as f64))
+    }
+}
+
+
+impl Upcast<i64x2> for i32x4 {
+    // Blocked by stdsimd
+    // #[inline(always)]
+    // #[cfg(target_feature = "sse4.1")]
+    // fn upcast(self) -> (i64x2, i64x2) {
+    //     unsafe { (_mm_cvtepi32_epi64(self), _mm_cvtepi32_epi64(_mm_shuffle_epi32(0x0E))) }
+    // }
+
+    #[inline(always)]
+    // #[cfg(not(target_feature = "sse4.1"))]
+    fn upcast(self) -> (i64x2, i64x2) {
+        (i64x2::new(self.extract(0) as i64,
+                    self.extract(1) as i64),
+         i64x2::new(self.extract(2) as i64,
+                    self.extract(3) as i64))
+    }
+}
+
+
+impl Upcast<u64x2> for u32x4 {
+    // Blocked by stdsimd
+    // #[inline(always)]
+    // #[cfg(target_feature = "sse4.1")]
+    // fn upcast(self) -> (u64x2, u64x2) {
+    //     unsafe { (_mm_cvtepu32_epu64(self), _mm_cvtepu32_epu64(_mm_shuffle_epu32(0x0E))) }
+    // }
+
+    #[inline(always)]
+    // #[cfg(not(target_feature = "sse4.1"))]
+    fn upcast(self) -> (u64x2, u64x2) {
+        (u64x2::new(self.extract(0) as u64,
+                    self.extract(1) as u64),
+         u64x2::new(self.extract(2) as u64,
+                    self.extract(3) as u64))
+    }
+}
+
+impl Upcast<f64x4> for f32x8 {
+    #[inline(always)]
+    #[cfg(target_feature = "avx2")]
+    fn upcast(self) -> (f64x4, f64x4) {
+        // Shuffle the vector as i32s for better perf
+        unsafe { (_mm256_cvtps_pd(self), _mm256_cvtps_pd(_mm256_shuffle_epi32(self.be_i32s(), 0x0E).be_f32s_unchecked())) }
+    }
+
+    #[inline(always)]
+    #[cfg(not(target_feature = "avx2"))]
+    fn upcast(self) -> (f64x4, f64x4) {
+        (f64x4::new(self.extract(0) as f64,
+                    self.extract(1) as f64,
+                    self.extract(2) as f64,
+                    self.extract(3) as f64),
+         f64x4::new(self.extract(4) as f64,
+                    self.extract(5) as f64,
+                    self.extract(6) as f64,
+                    self.extract(7) as f64))
+    }
+}
+
+impl Upcast<f64x4> for i32x8 {
+    #[inline(always)]
+    #[cfg(target_feature = "avx2")]
+    fn upcast(self) -> (f64x4, f64x4) {
+        unsafe { (_mm256_cvtepi32_pd(self), _mm256_cvtepi32_pd(_mm256_shuffle_epi32(self, 0x0E))) }
+    }
+
+    #[inline(always)]
+    #[cfg(not(target_feature = "avx2"))]
+    fn upcast(self) -> (f64x4, f64x4) {
+        (f64x4::new(self.extract(0) as f64,
+                    self.extract(1) as f64,
+                    self.extract(2) as f64,
+                    self.extract(3) as f64),
+         f64x4::new(self.extract(4) as f64,
+                    self.extract(5) as f64,
+                    self.extract(6) as f64,
+                    self.extract(7) as f64))
+    }
+}
+
+
+impl Upcast<i64x4> for i32x8 {
+    // Blocked by stdsimd
+    // #[inline(always)]
+    // #[cfg(target_feature = "avx2")]
+    // fn upcast(self) -> (i64x4, i64x4) {
+    //     unsafe { (_mm256_cvtepi32_epi64(self), _mm256_cvtepi32_epi64(_mm256_shuffle_epi32(0x0E))) }
+    // }
+
+    #[inline(always)]
+    // #[cfg(not(target_feature = "avx2"))]
+    fn upcast(self) -> (i64x4, i64x4) {
+        (i64x4::new(self.extract(0) as i64,
+                    self.extract(1) as i64,
+                    self.extract(2) as i64,
+                    self.extract(3) as i64),
+         i64x4::new(self.extract(4) as i64,
+                    self.extract(5) as i64,
+                    self.extract(6) as i64,
+                    self.extract(7) as i64))
+    }
+}
+
+
+impl Upcast<u64x4> for u32x8 {
+    // Blocked by stdsimd
+    // #[inline(always)]
+    // #[cfg(target_feature = "avx2")]
+    // fn upcast(self) -> (u64x4, u64x4) {
+    //     unsafe { (_mm256_cvtepu32_epu64(self), _mm256_cvtepu32_epu64(_mm256_shuffle_epu32(0x0E))) }
+    // }
+
+    #[inline(always)]
+    // #[cfg(not(target_feature = "avx2"))]
+    fn upcast(self) -> (u64x4, u64x4) {
+        (u64x4::new(self.extract(0) as u64,
+                    self.extract(1) as u64,
+                    self.extract(2) as u64,
+                    self.extract(3) as u64),
+         u64x4::new(self.extract(4) as u64,
+                    self.extract(5) as u64,
+                    self.extract(6) as u64,
+                    self.extract(7) as u64))
+    }
+}
+
+impl Upcast<f64x8> for f32x16 {
+    #[inline(always)]
+    fn upcast(self) -> (f64x8, f64x8) {
+        (f64x8::new(self.extract(0) as f64,
+                    self.extract(1) as f64,
+                    self.extract(2) as f64,
+                    self.extract(3) as f64,
+                    self.extract(4) as f64,
+                    self.extract(5) as f64,
+                    self.extract(6) as f64,
+                    self.extract(7) as f64),
+         f64x8::new(self.extract(8) as f64,
+                    self.extract(9) as f64,
+                    self.extract(10) as f64,
+                    self.extract(11) as f64,
+                    self.extract(12) as f64,
+                    self.extract(13) as f64,
+                    self.extract(14) as f64,
+                    self.extract(15) as f64))
+    }
+}
+
+impl Upcast<f64x8> for i32x16 {
+    #[inline(always)]
+    fn upcast(self) -> (f64x8, f64x8) {
+        (f64x8::new(self.extract(0) as f64,
+                    self.extract(1) as f64,
+                    self.extract(2) as f64,
+                    self.extract(3) as f64,
+                    self.extract(4) as f64,
+                    self.extract(5) as f64,
+                    self.extract(6) as f64,
+                    self.extract(7) as f64),
+         f64x8::new(self.extract(8) as f64,
+                    self.extract(9) as f64,
+                    self.extract(10) as f64,
+                    self.extract(11) as f64,
+                    self.extract(12) as f64,
+                    self.extract(13) as f64,
+                    self.extract(14) as f64,
+                    self.extract(15) as f64))
+    }
+}
+
+
+impl Upcast<i64x8> for i32x16 {
+    #[inline(always)]
+    fn upcast(self) -> (i64x8, i64x8) {
+        (i64x8::new(self.extract(0) as i64,
+                    self.extract(1) as i64,
+                    self.extract(2) as i64,
+                    self.extract(3) as i64,
+                    self.extract(4) as i64,
+                    self.extract(5) as i64,
+                    self.extract(6) as i64,
+                    self.extract(7) as i64),
+         i64x8::new(self.extract(8) as i64,
+                    self.extract(9) as i64,
+                    self.extract(10) as i64,
+                    self.extract(11) as i64,
+                    self.extract(12) as i64,
+                    self.extract(13) as i64,
+                    self.extract(14) as i64,
+                    self.extract(15) as i64))
+    }
+}
+
+
+impl Upcast<u64x8> for u32x16 {
+    #[inline(always)]
+    fn upcast(self) -> (u64x8, u64x8) {
+        (u64x8::new(self.extract(0) as u64,
+                    self.extract(1) as u64,
+                    self.extract(2) as u64,
+                    self.extract(3) as u64,
+                    self.extract(4) as u64,
+                    self.extract(5) as u64,
+                    self.extract(6) as u64,
+                    self.extract(7) as u64),
+         u64x8::new(self.extract(8) as u64,
+                    self.extract(9) as u64,
+                    self.extract(10) as u64,
+                    self.extract(11) as u64,
+                    self.extract(12) as u64,
+                    self.extract(13) as u64,
+                    self.extract(14) as u64,
+                    self.extract(15) as u64))
+    }
+}
