7cd26da94a1403a903694f35334af73cbb18930d
Adam Niederer <adam.niederer@gmail.com>
2017-11-18 22:04:32 -0500

    Add halfs, intereleave to vector types

--- src/vecs.rs
+++ src/vecs.rs
@@ -48,6 +48,8 @@ pub trait Packed<T : Packable> : Sized {
     fn load(data: &[T], offset: usize) -> Self;
     fn store(self, data: &mut [T], offset: usize);
     fn splat(data: T) -> Self;
+    fn halfs(hi: T, lo: T) -> Self;
+    fn interleave(hi: T, lo: T) -> Self;
 }
 
 // A type which may be packed into a SIMD vector
@@ -57,7 +59,9 @@ pub trait Packable where Self : Sized + Copy {
 }
 
 macro_rules! impl_packed {
-    ($el:ty, $pvec:tt, $vec:tt, $sz:expr, $feat:expr, $nfeat:expr) => (
+    ($el:ty, $pvec:tt, $vec:tt, $sz:expr, $feat:expr, $nfeat:expr,
+     halfs => $halfs:expr,
+     interleave => $interleave:expr) => (
         #[allow(non_camel_case_types)]
         #[cfg(all(target_feature = $feat, not(target_feature = $nfeat)))]
         pub type $pvec = $vec;
@@ -68,7 +72,6 @@ macro_rules! impl_packed {
             type Vector = $vec;
         }
 
-        #[cfg(all(target_feature = $feat, not(target_feature = $nfeat)))]
         impl Packed<$el> for $vec {
             const WIDTH: usize = $sz;
 
@@ -83,6 +86,14 @@ macro_rules! impl_packed {
             #[inline(always)]
             fn splat(data: $el) -> Self {
                 $vec::splat(data)
+            }
+            #[inline(always)]
+            fn halfs(hi: $el, lo: $el) -> Self {
+                $halfs(hi, lo)
+            }
+            #[inline(always)]
+            fn interleave(hi: $el, lo: $el) -> Self {
+                $interleave(hi, lo)
             }
         }
 
@@ -141,42 +152,162 @@ macro_rules! impl_packed {
     );
 }
 
-impl_packed!(u8, u8s, u8x64, 64, "avx512", "avx1024");
-impl_packed!(u8, u8s, u8x32, 32, "avx2", "avx512");
-impl_packed!(u8, u8s, u8x16, 16, "sse", "avx2");
+impl_packed! {
+    u8, u8s, u8x64, 64, "avx512", "avx1024",
+    halfs => |hi, lo| { u8x64::new(hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo) },
+    interleave => |hi, lo| { u8x64::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    u8, u8s, u8x32, 32, "avx2", "avx512",
+    halfs => |hi, lo| { u8x32::new(hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo) },
+    interleave => |hi, lo| { u8x32::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    u8, u8s, u8x16, 16, "sse", "avx2",
+    halfs => |hi, lo| { u8x16::new(hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo) },
+    interleave => |hi, lo| { u8x16::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
+}
 
-impl_packed!(i8, i8s, i8x64, 64, "avx512", "avx1024");
-impl_packed!(i8, i8s, i8x32, 32, "avx2", "avx512");
-impl_packed!(i8, i8s, i8x16, 16, "sse", "avx2");
+impl_packed! {
+    i8, i8s, i8x64, 64, "avx512", "avx1024",
+    halfs => |hi, lo| { i8x64::new(hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo) },
+    interleave => |hi, lo| { i8x64::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    i8, i8s, i8x32, 32, "avx2", "avx512",
+    halfs => |hi, lo| { i8x32::new(hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo) },
+    interleave => |hi, lo| { i8x32::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    i8, i8s, i8x16, 16, "sse", "avx2",
+    halfs => |hi, lo| { i8x16::new(hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo) },
+    interleave => |hi, lo| { i8x16::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
+}
 
-impl_packed!(u16, u16s, u16x32, 32, "avx512", "avx1024");
-impl_packed!(u16, u16s, u16x16, 16, "avx2", "avx512");
-impl_packed!(u16, u16s, u16x8, 8, "sse", "avx2");
+impl_packed! {
+    u16, u16s, u16x32, 32, "avx512", "avx1024",
+    halfs => |hi, lo| { u16x32::new(hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo) },
+    interleave => |hi, lo| { u16x32::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    u16, u16s, u16x16, 16, "avx2", "avx512",
+    halfs => |hi, lo| { u16x16::new(hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo) },
+    interleave => |hi, lo| { u16x16::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    u16, u16s, u16x8, 8, "sse", "avx2",
+    halfs => |hi, lo| { u16x8::new(hi, hi, hi, hi, lo, lo, lo, lo) },
+    interleave => |hi, lo| { u16x8::new(hi, lo, hi, lo, hi, lo, hi, lo) }
+}
 
-impl_packed!(i16, i16s, i16x32, 32, "avx512", "avx1024");
-impl_packed!(i16, i16s, i16x16, 16, "avx2", "avx512");
-impl_packed!(i16, i16s, i16x8, 8, "sse", "avx2");
+impl_packed! {
+    i16, i16s, i16x32, 32, "avx512", "avx1024",
+    halfs => |hi, lo| { i16x32::new(hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo, lo) },
+    interleave => |hi, lo| { i16x32::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    i16, i16s, i16x16, 16, "avx2", "avx512",
+    halfs => |hi, lo| { i16x16::new(hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo) },
+    interleave => |hi, lo| { i16x16::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    i16, i16s, i16x8, 8, "sse", "avx2",
+    halfs => |hi, lo| { i16x8::new(hi, hi, hi, hi, lo, lo, lo, lo) },
+    interleave => |hi, lo| { i16x8::new(hi, lo, hi, lo, hi, lo, hi, lo) }
+}
 
-impl_packed!(u32, u32s, u32x16, 16, "avx512", "avx1024");
-impl_packed!(u32, u32s, u32x8, 8, "avx2", "avx512");
-impl_packed!(u32, u32s, u32x4, 4, "sse", "avx2");
+impl_packed! {
+    u32, u32s, u32x16, 16, "avx512", "avx1024",
+    halfs => |hi, lo| { u32x16::new(hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo) },
+    interleave => |hi, lo| { u32x16::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    u32, u32s, u32x8, 8, "avx2", "avx512",
+    halfs => |hi, lo| { u32x8::new(hi, hi, hi, hi, lo, lo, lo, lo) },
+    interleave => |hi, lo| { u32x8::new(hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    u32, u32s, u32x4, 4, "sse", "avx2",
+    halfs => |hi, lo| { u32x4::new(hi, hi, lo, lo) },
+    interleave => |hi, lo| { u32x4::new(hi, lo, hi, lo) }
+}
 
-impl_packed!(i32, i32s, i32x16, 16, "avx512", "avx1024");
-impl_packed!(i32, i32s, i32x8, 8, "avx2", "avx512");
-impl_packed!(i32, i32s, i32x4, 4, "sse", "avx2");
+impl_packed! {
+    i32, i32s, i32x16, 16, "avx512", "avx1024",
+    halfs => |hi, lo| { i32x16::new(hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo) },
+    interleave => |hi, lo| { i32x16::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    i32, i32s, i32x8, 8, "avx2", "avx512",
+    halfs => |hi, lo| { i32x8::new(hi, hi, hi, hi, lo, lo, lo, lo) },
+    interleave => |hi, lo| { i32x8::new(hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    i32, i32s, i32x4, 4, "sse", "avx2",
+    halfs => |hi, lo| { i32x4::new(hi, hi, lo, lo) },
+    interleave => |hi, lo| { i32x4::new(hi, lo, hi, lo) }
+}
 
-impl_packed!(f32, f32s, f32x16, 16, "avx512", "avx1024");
-impl_packed!(f32, f32s, f32x8, 8, "avx2", "avx512");
-impl_packed!(f32, f32s, f32x4, 4, "sse", "avx2");
+impl_packed! {
+    f32, f32s, f32x16, 16, "avx512", "avx1024",
+    halfs => |hi, lo| { f32x16::new(hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo) },
+    interleave => |hi, lo| { f32x16::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    f32, f32s, f32x8, 8, "avx2", "avx512",
+    halfs => |hi, lo| { f32x8::new(hi, hi, hi, hi, lo, lo, lo, lo) },
+    interleave => |hi, lo| { f32x8::new(hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    f32, f32s, f32x4, 4, "sse", "avx2",
+    halfs => |hi, lo| { f32x4::new(hi, hi, lo, lo) },
+    interleave => |hi, lo| { f32x4::new(hi, lo, hi, lo) }
+}
 
-impl_packed!(u64, u64s, u64x8, 8, "avx512", "avx1024");
-impl_packed!(u64, u64s, u64x4, 4, "avx2", "avx512");
-impl_packed!(u64, u64s, u64x2, 2, "sse", "avx2");
+impl_packed! {
+    u64, u64s, u64x8, 8, "avx512", "avx1024",
+    halfs => |hi, lo| { u64x8::new(hi, hi, hi, hi, lo, lo, lo, lo) },
+    interleave => |hi, lo| { u64x8::new(hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    u64, u64s, u64x4, 4, "avx2", "avx512",
+    halfs => |hi, lo| { u64x4::new(hi, hi, lo, lo) },
+    interleave => |hi, lo| { u64x4::new(hi, lo, hi, lo) }
+}
+impl_packed! {
+    u64, u64s, u64x2, 2, "sse", "avx2",
+    halfs => |hi, lo| { u64x2::new(hi, lo) },
+    interleave => |hi, lo| { u64x2::new(hi, lo) }
+}
 
-impl_packed!(i64, i64s, i64x8, 8, "avx512", "avx1024");
-impl_packed!(i64, i64s, i64x4, 4, "avx2", "avx512");
-impl_packed!(i64, i64s, i64x2, 2, "sse", "avx2");
+impl_packed! {
+    i64, i64s, i64x8, 8, "avx512", "avx1024",
+    halfs => |hi, lo| { i64x8::new(hi, hi, hi, hi, lo, lo, lo, lo) },
+    interleave => |hi, lo| { i64x8::new(hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    i64, i64s, i64x4, 4, "avx2", "avx512",
+    halfs => |hi, lo| { i64x4::new(hi, hi, lo, lo) },
+    interleave => |hi, lo| { i64x4::new(hi, lo, hi, lo) }
+}
+impl_packed! {
+    i64, i64s, i64x2, 2, "sse", "avx2",
+    halfs => |hi, lo| { i64x2::new(hi, lo) },
+    interleave => |hi, lo| { i64x2::new(hi, lo) }
+}
 
-impl_packed!(f64, f64s, f64x8, 8, "avx512", "avx1024");
-impl_packed!(f64, f64s, f64x4, 4, "avx2", "avx512");
-impl_packed!(f64, f64s, f64x2, 2, "sse", "avx2");
+impl_packed! {
+    f64, f64s, f64x8, 8, "avx512", "avx1024",
+    halfs => |hi, lo| { f64x8::new(hi, hi, hi, hi, lo, lo, lo, lo) },
+    interleave => |hi, lo| { f64x8::new(hi, lo, hi, lo, hi, lo, hi, lo) }
+}
+impl_packed! {
+    f64, f64s, f64x4, 4, "avx2", "avx512",
+    halfs => |hi, lo| { f64x4::new(hi, hi, lo, lo) },
+    interleave => |hi, lo| { f64x4::new(hi, lo, hi, lo) }
+}
+impl_packed! {
+    f64, f64s, f64x2, 2, "sse", "avx2",
+    halfs => |hi, lo| { f64x2::new(hi, lo) },
+    interleave => |hi, lo| { f64x2::new(hi, lo) }
+}
