bf69f3d20880ad65c86628d1dace1e6b01a0e3ca
Adam Niederer <adam.niederer@gmail.com>
2017-11-19 00:26:58 -0500

    Add tests for recip

--- src/intrin/recip.rs
+++ src/intrin/recip.rs
@@ -23,3 +23,22 @@ rust_fallback_impl! {
         recip => _mm_rcp_ps(), [0, 1, 2, 3];
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use vecs::*;
+    use intrin::*;
+    use std::f32::INFINITY;
+
+    #[test]
+    fn recip_f32s() {
+        let mut i = -1024.0;
+        while i < 1024.0 {
+            // This test has some pretty significant float error if done on x86
+            let ans = f32s::splat(i).recip().extract(0);
+            let real = f32s::splat(1.0 / i).extract(0);
+            assert!((real == INFINITY && ans == INFINITY) || (ans - real).abs() < 0.0005);
+            i += 1.0
+        }
+    }
+}
