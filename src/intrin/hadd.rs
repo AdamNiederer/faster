ba1ad7cc489d8f98f739d6d3ce5aff76dcd57068
Adam Niederer <adam.niederer@gmail.com>
2017-11-18 22:35:12 -0500

    Make hadd behave the same on all platforms
    Now, hadd will produce interleaved output instead of what intel says will happen.

--- src/intrin/hadd.rs
+++ src/intrin/hadd.rs
@@ -7,6 +7,7 @@
 
 use stdsimd::vendor::*;
 use stdsimd::simd::*;
+use intrin::transmute::*;
 
 pub trait PackedHadd {
     fn hadd(&self, other: Self) -> Self;
@@ -16,15 +17,16 @@ impl PackedHadd for f32x4 {
     #[inline(always)]
     #[cfg(target_feature = "sse2")]
     fn hadd(&self, other: Self) -> Self {
-        unsafe { _mm_hadd_ps(*self, other) }
+        unsafe { _mm_hadd_ps(_mm_shuffle_ps(*self, other, 0b01000100),
+                             _mm_shuffle_ps(*self, other, 0b11101110)) }
     }
 
     #[inline(always)]
     #[cfg(not(target_feature = "sse2"))]
     fn hadd(&self, other: Self) -> Self {
         Self::new(self.extract(0) + self.extract(1),
-                  self.extract(2) + self.extract(3),
                   other.extract(0) + other.extract(1),
+                  self.extract(2) + self.extract(3),
                   other.extract(2) + other.extract(3))
     }
 }
@@ -46,22 +48,23 @@ impl PackedHadd for f64x2 {
 
 impl PackedHadd for f32x8 {
     #[inline(always)]
-    #[cfg(target_feature = "avx")]
+    #[cfg(target_feature = "avx2")]
     fn hadd(&self, other: Self) -> Self {
-        unsafe { _mm256_hadd_ps(*self, other) }
+        unsafe { _mm256_hadd_ps(_mm256_unpacklo_epi64(self.be_i64s(), other.be_i64s()).be_f32s_unchecked(),
+                                _mm256_unpackhi_epi64(self.be_i64s(), other.be_i64s()).be_f32s_unchecked()) }
     }
 
     #[inline(always)]
-    #[cfg(not(target_feature = "avx"))]
+    #[cfg(not(target_feature = "avx2"))]
     fn hadd(&self, other: Self) -> Self {
         Self::new(self.extract(0) + self.extract(1),
-                  self.extract(2) + self.extract(3),
                   other.extract(0) + other.extract(1),
+                  self.extract(2) + self.extract(3),
                   other.extract(2) + other.extract(3),
                   self.extract(4) + self.extract(5),
-                  self.extract(6) + self.extract(7),
                   other.extract(4) + other.extract(5),
-                  other.extract(6) + other.extract(7))    }
+                  self.extract(6) + self.extract(7),
+                  other.extract(6) + other.extract(7)) }
 }
 
 impl PackedHadd for f64x4 {
@@ -85,19 +88,20 @@ impl PackedHadd for i16x8 {
     #[inline(always)]
     #[cfg(target_feature = "ssse3")]
     fn hadd(&self, other: Self) -> Self {
-        unsafe { _mm_hadd_epi16(*self, other) }
+        unsafe { _mm_hadd_epi16(_mm_unpacklo_epi32(self.be_i32s(), other.be_i32s()).be_i16s(),
+                                _mm_unpackhi_epi32(self.be_i32s(), other.be_i32s()).be_i16s()) }
     }
 
     #[inline(always)]
     #[cfg(not(target_feature = "ssse3"))]
     fn hadd(&self, other: Self) -> Self {
         Self::new(self.extract(0) + self.extract(1),
-                  self.extract(2) + self.extract(3),
-                  self.extract(4) + self.extract(5),
-                  self.extract(6) + self.extract(7),
                   other.extract(0) + other.extract(1),
+                  self.extract(2) + self.extract(3),
                   other.extract(2) + other.extract(3),
+                  self.extract(4) + self.extract(5),
                   other.extract(4) + other.extract(5),
+                  self.extract(6) + self.extract(7),
                   other.extract(6) + other.extract(7))
     }
 }
@@ -106,44 +110,46 @@ impl PackedHadd for i32x4 {
     #[inline(always)]
     #[cfg(target_feature = "ssse3")]
     fn hadd(&self, other: Self) -> Self {
-        unsafe { _mm_hadd_epi32(*self, other) }
+        unsafe { _mm_hadd_epi32(_mm_unpacklo_epi64(self.be_i64s(), other.be_i64s()).be_i32s(),
+                                _mm_unpackhi_epi64(self.be_i64s(), other.be_i64s()).be_i32s()) }
     }
 
     #[inline(always)]
     #[cfg(not(target_feature = "ssse3"))]
     fn hadd(&self, other: Self) -> Self {
         Self::new(self.extract(0) + self.extract(1),
-                  self.extract(2) + self.extract(3),
                   other.extract(0) + other.extract(1),
+                  self.extract(2) + self.extract(3),
                   other.extract(2) + other.extract(3))
     }
 }
 
-#[cfg(target_feature = "avx2")]
 impl PackedHadd for i16x16 {
+    #[cfg(target_feature = "avx2")]
     #[inline(always)]
     fn hadd(&self, other: Self) -> Self {
-        unsafe { _mm256_hadd_epi16(*self, other) }
+        unsafe { _mm256_hadd_epi16(_mm256_unpacklo_epi32(self.be_i32s(), other.be_i32s()).be_i16s(),
+                                   _mm256_unpackhi_epi32(self.be_i32s(), other.be_i32s()).be_i16s()) }
     }
 
     #[inline(always)]
     #[cfg(not(target_feature = "avx2"))]
     fn hadd(&self, other: Self) -> Self {
         Self::new(self.extract(0) + self.extract(1),
-                  self.extract(2) + self.extract(3),
-                  self.extract(4) + self.extract(5),
-                  self.extract(6) + self.extract(7),
                   other.extract(0) + other.extract(1),
+                  self.extract(2) + self.extract(3),
                   other.extract(2) + other.extract(3),
+                  self.extract(4) + self.extract(5),
                   other.extract(4) + other.extract(5),
+                  self.extract(6) + self.extract(7),
                   other.extract(6) + other.extract(7),
                   self.extract(8) + self.extract(9),
-                  self.extract(10) + self.extract(11),
-                  self.extract(12) + self.extract(13),
-                  self.extract(14) + self.extract(15),
                   other.extract(8) + other.extract(9),
+                  self.extract(10) + self.extract(11),
                   other.extract(10) + other.extract(11),
+                  self.extract(12) + self.extract(13),
                   other.extract(12) + other.extract(13),
+                  self.extract(14) + self.extract(15),
                   other.extract(14) + other.extract(15))
     }
 }
@@ -153,19 +159,53 @@ impl PackedHadd for i32x8 {
     #[inline(always)]
     #[cfg(target_feature = "avx2")]
     fn hadd(&self, other: Self) -> Self {
-        unsafe { _mm256_hadd_epi32(*self, other) }
+        unsafe { _mm256_hadd_epi32(_mm256_unpacklo_epi64(self.be_i64s(), other.be_i64s()).be_i32s(),
+                                   _mm256_unpackhi_epi64(self.be_i64s(), other.be_i64s()).be_i32s()) }
     }
 
     #[inline(always)]
     #[cfg(not(target_feature = "avx2"))]
     fn hadd(&self, other: Self) -> Self {
         Self::new(self.extract(0) + self.extract(1),
-                  self.extract(2) + self.extract(3),
                   other.extract(0) + other.extract(1),
+                  self.extract(2) + self.extract(3),
                   other.extract(2) + other.extract(3),
                   self.extract(4) + self.extract(5),
-                  self.extract(6) + self.extract(7),
                   other.extract(4) + other.extract(5),
+                  self.extract(6) + self.extract(7),
                   other.extract(6) + other.extract(7))
     }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use vecs::*;
+    use intrin::*;
+
+    #[test]
+    fn hadd_i16s() {
+        assert_eq!(i16s::splat(1).hadd(i16s::splat(2)), i16s::interleave(2, 4));
+        assert_eq!(i16s::interleave(1, 2).hadd(i16s::interleave(3, 4)), i16s::interleave(3, 7));
+    }
+
+    #[test]
+    fn hadd_i32s() {
+        assert_eq!(i32s::splat(1).hadd(i32s::splat(2)), i32s::interleave(2, 4));
+        assert_eq!(i32s::interleave(1, 2).hadd(i32s::interleave(3, 4)), i32s::interleave(3, 7));
+    }
+
+    #[test]
+    fn hadd_f32s() {
+        assert_eq!(f32s::splat(1.0).hadd(f32s::splat(2.0)), f32s::interleave(2.0, 4.0));
+        assert_eq!(f32s::interleave(1.0, 2.0).hadd(f32s::interleave(3.0, 4.0)), f32s::interleave(3.0, 7.0));
+    }
+
+    #[test]
+    fn hadd_f64s() {
+        assert_eq!(f64s::splat(1.0).hadd(f64s::splat(2.0)), f64s::interleave(2.0, 4.0));
+        assert_eq!(f64s::interleave(1.0, 2.0).hadd(f64s::interleave(3.0, 4.0)), f64s::interleave(3.0, 7.0));
+    }
+
+
 }
