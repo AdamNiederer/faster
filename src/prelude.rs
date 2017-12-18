4d897edb823604fd8b508b8c70e6469d2bd795f9
Adam Niederer <adam.niederer@gmail.com>
2017-12-17 22:10:02 -0500

    Add prelude

---
+++ src/prelude.rs
@@ -0,0 +1,11 @@
+// This file is part of faster, the SIMD library for humans.
+// Copyright 2017 Adam Niederer <adam.niederer@gmail.com>
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+pub use iters::*;
+pub use vecs::{Packed};
+pub use vecs::{u8s, i8s, u16s, i16s, u32s, i32s, f32s, u64s, i64s, f64s};
+pub use intrin::*;
