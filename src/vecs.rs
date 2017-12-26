1a36076c0eeec4dc68d44942e348369922721d45
Adam Niederer <adam.niederer@gmail.com>
2017-12-26 00:32:48 -0500

    Add sum/product vec tests
    And fix bugs found with said tests

--- src/vecs.rs
+++ src/vecs.rs
@@ -238,7 +238,7 @@ impl_packed! {
     interleave => |hi, lo| { u16x32::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
 }
 impl_packed! {
-    u16, u16s, u16x16, 2, 2, ["avx"], ["avx512"],
+    u16, u16s, u16x16, 2, 16, ["avx"], ["avx512"],
     halfs => |hi, lo| { u16x16::new(hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo) },
     interleave => |hi, lo| { u16x16::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
 }
@@ -254,7 +254,7 @@ impl_packed! {
     interleave => |hi, lo| { i16x32::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
 }
 impl_packed! {
-    i16, i16s, i16x16, 2, 2, ["avx"], ["avx512"],
+    i16, i16s, i16x16, 2, 16, ["avx"], ["avx512"],
     halfs => |hi, lo| { i16x16::new(hi, hi, hi, hi, hi, hi, hi, hi, lo, lo, lo, lo, lo, lo, lo, lo) },
     interleave => |hi, lo| { i16x16::new(hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo, hi, lo) }
 }
@@ -358,4 +358,47 @@ impl_packed! {
     f64, f64s, f64x2, 8, 2, [], ["avx"],
     halfs => |hi, lo| { f64x2::new(hi, lo) },
     interleave => |hi, lo| { f64x2::new(hi, lo) }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::Packed;
+    use super::*;
+
+    macro_rules! test_sum {
+        (($($el:tt),*), ($($vec:tt),*), ($($fn:tt),*), ($($sum:tt),*)) => (
+            $(
+                #[test]
+                fn $fn() {
+                    assert_eq!($vec::splat(1i8 as $el).sum(), $sum as $el);
+                }
+            )*
+        )
+    }
+
+    macro_rules! test_product {
+        (($($el:tt),*), ($($vec:tt),*), ($($fn:tt),*), ($($sum:tt),*)) => (
+            $(
+                #[test]
+                fn $fn() {
+                    assert_eq!($vec::splat(1i8 as $el).product(), $sum as $el);
+                }
+            )*
+        )
+    }
+
+    test_sum!((u8, u8, u8, i8, i8, i8, u16, u16, u16, i16, i16, i16, u32, u32, u32, i32, i32, i32, f32, f32, f32, u64, u64, u64, i64, i64, i64, f64, f64, f64),
+              (u8x64, u8x32, u8x16, i8x64, i8x32, i8x16, u16x32, u16x16, u16x8, i16x32, i16x16, i16x8, u32x16, u32x8, u32x4, i32x16, i32x8, i32x4, f32x16, f32x8, f32x4, u64x8, u64x4, u64x2, i64x8, i64x4, i64x2, f64x8, f64x4, f64x2),
+              (scalar_sum_u8x64, scalar_sum_u8x32, scalar_sum_u8x16, scalar_sum_i8x64, scalar_sum_i8x32, scalar_sum_i8x16, scalar_sum_u16x32, scalar_sum_u16x16, scalar_sum_u16x8, scalar_sum_i16x32, scalar_sum_i16x16, scalar_sum_i16x8, scalar_sum_u32x16, scalar_sum_u32x8, scalar_sum_u32x4, scalar_sum_i32x16, scalar_sum_i32x8, scalar_sum_i32x4, scalar_sum_f32x16, scalar_sum_f32x8, scalar_sum_f32x4, scalar_sum_u64x8, scalar_sum_u64x4, scalar_sum_u64x2, scalar_sum_i64x8, scalar_sum_i64x4, scalar_sum_i64x2, scalar_sum_f64x8, scalar_sum_f64x4, scalar_sum_f64x2),
+              (64, 32, 16, 64, 32, 16, 32, 16, 8, 32, 16, 8, 16, 8, 4, 16, 8, 4, 16, 8, 4, 8, 4, 2, 8, 4, 2, 8, 4, 2));
+
+    // TODO: Do we need better test cases for this?
+    test_product!((u8, u8, u8, i8, i8, i8, u16, u16, u16, i16, i16, i16, u32, u32, u32, i32, i32, i32, f32, f32, f32, u64, u64, u64, i64, i64, i64, f64, f64, f64),
+                  (u8x64, u8x32, u8x16, i8x64, i8x32, i8x16, u16x32, u16x16, u16x8, i16x32, i16x16, i16x8, u32x16, u32x8, u32x4, i32x16, i32x8, i32x4, f32x16, f32x8, f32x4, u64x8, u64x4, u64x2, i64x8, i64x4, i64x2, f64x8, f64x4, f64x2),
+                  (scalar_product_u8x64, scalar_product_u8x32, scalar_product_u8x16, scalar_product_i8x64, scalar_product_i8x32, scalar_product_i8x16, scalar_product_u16x32, scalar_product_u16x16, scalar_product_u16x8, scalar_product_i16x32, scalar_product_i16x16, scalar_product_i16x8, scalar_product_u32x16, scalar_product_u32x8, scalar_product_u32x4, scalar_product_i32x16, scalar_product_i32x8, scalar_product_i32x4, scalar_product_f32x16, scalar_product_f32x8, scalar_product_f32x4, scalar_product_u64x8, scalar_product_u64x4, scalar_product_u64x2, scalar_product_i64x8, scalar_product_i64x4, scalar_product_i64x2, scalar_product_f64x8, scalar_product_f64x4, scalar_product_f64x2),
+                  (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1));
+
+
 }
