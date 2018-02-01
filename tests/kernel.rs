e5f2aade4a5b86c2323af51c3143747205a53b63
Adam Niederer <adam.niederer@gmail.com>
2018-01-31 21:37:09 -0500

    Fix test whitespace and imports

--- tests/kernel.rs
+++ tests/kernel.rs
@@ -4,21 +4,19 @@ extern crate faster;
 
 #[cfg(test)]
 mod tests {
-    use faster::{IntoPackedRefIterator, IntoPackedZip, PackedZippedIterator, PackedIterator, Packed, 
-                 f64s, f32s, i64s, i32s, i16s, i8s, u64s, u32s, u16s, u8s};
-    
-    
+    use faster::*;
+
     macro_rules! kernel_definite {
         ($name:ident, $native_type:ty, $simd_type:ident) => (
-        
-            /// Tests a number of simple kernel computations with integer values. 
+
+            /// Tests a number of simple kernel computations with integer values.
             #[test]
             fn $name() {
                 for n in 0 .. 16 {
-                
-                    let vec_of_1 = vec![1 as $native_type; n];                    
-                    let vec_of_3 = vec![3 as $native_type; n];                    
-        
+
+                    let vec_of_1 = vec![1 as $native_type; n];
+                    let vec_of_3 = vec![3 as $native_type; n];
+
                     // Should produce n times (1 - 3) * (1 - 3) == n * 4 for each element
                     let sum: $native_type = ((&vec_of_1[..]).simd_iter(), (&vec_of_3[..]).simd_iter()).zip()
                         .simd_map(($simd_type(0), $simd_type(0)), |(a, b)| (a - b) * (a - b) )
@@ -35,32 +33,32 @@ mod tests {
 
                     assert_eq!(sum, (n * 4) as $native_type);
                 }
-            }    
+            }
         )
     }
-    
+
     kernel_definite!(kernel_i64, i64, i64s);
     kernel_definite!(kernel_i32, i32, i32s);
     kernel_definite!(kernel_i16, i16, i16s);
-    kernel_definite!(kernel_i8,  i8,  i8s);
+    kernel_definite!(kernel_i8, i8, i8s);
 
     kernel_definite!(kernel_u64, u64, u64s);
     kernel_definite!(kernel_u32, u32, u32s);
     kernel_definite!(kernel_u16, u16, u16s);
-    kernel_definite!(kernel_u8,  u8,  u8s);
+    kernel_definite!(kernel_u8, u8, u8s);
 
-    
+
     macro_rules! kernel_relative {
         ($name:ident, $native_type:ty, $simd_type:ident) => (
-        
-            /// Tests a number of simple kernel computations with float values. 
+
+            /// Tests a number of simple kernel computations with float values.
             #[test]
             fn $name() {
                 for n in 0 .. 16 {
-                
-                    let vec_of_1 = vec![1 as $native_type; n];                    
-                    let vec_of_3 = vec![3 as $native_type; n];                    
-        
+
+                    let vec_of_1 = vec![1 as $native_type; n];
+                    let vec_of_3 = vec![3 as $native_type; n];
+
                     // Should produce n times (1 - 3) * (1 - 3) == n * 4 for each element
                     let sum_scalar: $native_type = ((&vec_of_1[..]).simd_iter(), (&vec_of_3[..]).simd_iter()).zip()
                         .simd_map(($simd_type(0.0 as $native_type), $simd_type(0.0 as $native_type)), |(a, b)| (a - b) * (a - b) )
@@ -74,16 +72,16 @@ mod tests {
                         .sum();
 
 
-                    // Ensure both ways produce the same result 
+                    // Ensure both ways produce the same result
                     assert_eq!(sum_scalar, sum_simd);
-                    
+
                     // Make sure the result is equal to our target within a certain limit.
                     assert!((sum_simd - (n * 4) as $native_type).abs() < 0.0001);
                 }
-            }    
+            }
         )
     }
 
-    kernel_relative!(kernel_f32,  f32,  f32s);
-    kernel_relative!(kernel_f64,  f64,  f64s);
+    kernel_relative!(kernel_f32, f32, f32s);
+    kernel_relative!(kernel_f64, f64, f64s);
 }
