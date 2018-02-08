6daaab552d19edee0b3f34aa1645925003e06c2d
Edward Barnard <eabarnard@gmail.com>
2018-02-08 18:46:15 +0000

    Implement SaturatingSub for u8s and u16s.

--- src/intrin/saturating_sub.rs
+++ src/intrin/saturating_sub.rs
@@ -13,10 +13,23 @@ pub trait SaturatingSub {
 }
 
 rust_fallback_impl_binary! {
+    impl SaturatingSub for u8x16 where "sse2" {
+        saturating_sub => _mm_subs_epu8(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    }
+}
+
+rust_fallback_impl_binary! {
     impl SaturatingSub for i8x16 where "sse2" {
         saturating_sub => _mm_subs_epi8(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     }
 }
+
+rust_fallback_impl_binary! {
+    impl SaturatingSub for u16x8 where "sse2" {
+        saturating_sub => _mm_subs_epu16(), [0, 1, 2, 3, 4, 5, 6, 7];
+    }
+}
+
 rust_fallback_impl_binary! {
     impl SaturatingSub for i16x8 where "sse2" {
         saturating_sub => _mm_subs_epi16(), [0, 1, 2, 3, 4, 5, 6, 7];
@@ -24,9 +37,22 @@ rust_fallback_impl_binary! {
 }
 
 rust_fallback_impl_binary! {
+    impl SaturatingSub for u8x32 where "avx2" {
+        saturating_sub => _mm256_subs_epu8(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+                                               17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    }
+}
+
+rust_fallback_impl_binary! {
     impl SaturatingSub for i8x32 where "avx2" {
         saturating_sub => _mm256_subs_epi8(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
                                                17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    }
+}
+
+rust_fallback_impl_binary! {
+    impl SaturatingSub for u16x16 where "avx2" {
+        saturating_sub => _mm256_subs_epu16(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     }
 }
 
