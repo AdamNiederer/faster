20424dfaa8a83af260b46034838f3a0867c07ba8
Adam Niederer <adam.niederer@gmail.com>
2018-02-12 22:28:06 -0500

    Implement popcnt for all vector widths

--- src/intrin/popcnt.rs
+++ src/intrin/popcnt.rs
@@ -17,35 +17,72 @@ pub trait Popcnt : Packed {
     }
 }
 
-macro_rules! impl_popcnt_avx2 {
-    ($($vec:ty),*) => {
+#[inline(always)]
+#[cfg(target_feature = "sse3")]
+fn popcnt128(v: u8x16) -> usize {
+    // SSE3 popcnt algorithm by Wojciech Muła
+    // http://wm.ite.pl/articles/sse-popcount.html
+    let lookup = u8x16::new(0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4);
+    let lo = v & u8x16::splat(0x0f);
+    let hi = v >> 4 & u8x16::splat(0x0f);
+    unsafe {
+        _mm_shuffle_epi8(lookup, hi) + _mm_shuffle_epi8(lookup, lo)
+    }.sum_upcast() as usize
+}
+
+#[inline(always)]
+#[cfg(not(target_feature = "sse3"))]
+fn popcnt128(v: u8x16) -> usize {
+    v.be_u64s(). scalar_reduce(0, |acc, s| acc + (s.count_ones() as usize))
+}
+
+#[inline(always)]
+#[cfg(target_feature = "avx2")]
+fn popcnt256(v: u8x32) -> usize {
+    // AVX2 popcnt algorithm by Wojciech Muła, Nathan Kurz, and Daniel Lemire
+    // https://arxiv.org/abs/1611.07612
+    let lookup = u8x32::new(0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4);
+    let lo = v.be_u8s() & u8x32::splat(0x0f);
+    let hi = (v.be_u8s() >> 4) & u8x32::splat(0x0f);
+    unsafe {
+        _mm256_shuffle_epi8(lookup, hi) + _mm256_shuffle_epi8(lookup, lo)
+    }.sum_upcast() as usize
+}
+
+#[inline(always)]
+#[cfg(not(target_feature = "avx2"))]
+fn popcnt256(v: u8x64) -> usize {
+    v.be_u64s().scalar_reduce(0, |acc, s| acc + (s.count_ones() as usize))
+}
+
+#[inline(always)]
+// #[cfg(not(target_feature = "avx512"))]
+fn popcnt512(v: u8x64) -> usize {
+    v.be_u64s().scalar_reduce(0, |acc, s| acc + (s.count_ones() as usize))
+}
+
+macro_rules! impl_popcnt {
+    ($($vec:ty, $fn:ident),*) => {
         $(
             impl Popcnt for $vec {
                 #[inline(always)]
-                #[cfg(target_feature = "avx2")]
                 fn count_ones(&self) -> usize {
-                    // AVX2 popcnt algorithm by Wojciech Muła, Nathan Kurz, and Daniel Lemire
-                    // https://arxiv.org/abs/1611.07612
-                    let lookup = u8x32::new(0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4);
-                    let lo = self.be_u8s() & u8x32::splat(0x0f);
-                    let hi = (self.be_u8s() >> 4) & u8x32::splat(0x0f);
-                    unsafe {
-                        (_mm256_shuffle_epi8(lookup, hi) + _mm256_shuffle_epi8(lookup, lo))
-                    }
-                    .sum_upcast() as usize
-                }
-
-                #[inline(always)]
-                #[cfg(not(target_feature = "avx2"))]
-                fn count_ones(&self) -> usize {
-                    self.scalar_reduce(0, |acc, s| acc + (s.count_ones() as usize))
+                    $fn(self.be_u8s())
                 }
             }
         )*
     }
 }
 
-impl_popcnt_avx2!(u8x32, i8x32, u16x16, i16x16, u32x8, i32x8, u64x4, i64x4);
+impl_popcnt!(u8x64, popcnt512, u8x32, popcnt256, u8x16, popcnt128,
+             i8x64, popcnt512, i8x32, popcnt256, i8x16, popcnt128,
+             u16x32, popcnt512, u16x16, popcnt256, u16x8, popcnt128,
+             i16x32, popcnt512, i16x16, popcnt256, i16x8, popcnt128,
+             u32x16, popcnt512, u32x8, popcnt256, u32x4, popcnt128,
+             i32x16, popcnt512, i32x8, popcnt256, i32x4, popcnt128,
+             u64x8, popcnt512, u64x4, popcnt256, u64x2, popcnt128,
+             i64x8, popcnt512, i64x4, popcnt256, i64x2, popcnt128);
+
 
 #[cfg(test)]
 mod tests {
@@ -65,11 +102,11 @@ mod tests {
         )
     }
 
-    test_popcnt!((u8, i8, u16, i16, u32, i32, u64, i64),
-                 (u8x32, i8x32, u16x16, i16x16, u32x8, i32x8, u64x4, i64x4),
-                 (popcnt_u8x32, popcnt_i8x32, popcnt_u16x16, popcnt_i16x16, popcnt_u32x8, popcnt_i32x8, popcnt_u64x4, popcnt_i64x4));
+    // test_popcnt!((u8, i8, u16, i16, u32, i32, u64, i64),
+    //              (u8x32, i8x32, u16x16, i16x16, u32x8, i32x8, u64x4, i64x4),
+    //              (popcnt_u8x32, popcnt_i8x32, popcnt_u16x16, popcnt_i16x16, popcnt_u32x8, popcnt_i32x8, popcnt_u64x4, popcnt_i64x4));
 
-    // test_popcnt!((u8, u8, u8, i8, i8, i8, u16, u16, u16, i16, i16, i16, u32, u32, u32, i32, i32, i32, u64, u64, u64, i64, i64, i64),
-    //              (u8x64, u8x32, u8x16, i8x64, i8x32, i8x16, u16x32, u16x16, u16x8, i16x32, i16x16, i16x8, u32x16, u32x8, u32x4, i32x16, i32x8, i32x4, u64x8, u64x4, u64x2, i64x8, i64x4, i64x2),
-    //              (popcnt_u8x64, popcnt_u8x32, popcnt_u8x16, popcnt_i8x64, popcnt_i8x32, popcnt_i8x16, popcnt_u16x32, popcnt_u16x16, popcnt_u16x8, popcnt_i16x32, popcnt_i16x16, popcnt_i16x8, popcnt_u32x16, popcnt_u32x8, popcnt_u32x4, popcnt_i32x16, popcnt_i32x8, popcnt_i32x4, popcnt_u64x8, popcnt_u64x4, popcnt_u64x2, popcnt_i64x8, popcnt_i64x4, popcnt_i64x2));
+    test_popcnt!((u8, u8, u8, i8, i8, i8, u16, u16, u16, i16, i16, i16, u32, u32, u32, i32, i32, i32, u64, u64, u64, i64, i64, i64),
+                 (u8x64, u8x32, u8x16, i8x64, i8x32, i8x16, u16x32, u16x16, u16x8, i16x32, i16x16, i16x8, u32x16, u32x8, u32x4, i32x16, i32x8, i32x4, u64x8, u64x4, u64x2, i64x8, i64x4, i64x2),
+                 (popcnt_u8x64, popcnt_u8x32, popcnt_u8x16, popcnt_i8x64, popcnt_i8x32, popcnt_i8x16, popcnt_u16x32, popcnt_u16x16, popcnt_u16x8, popcnt_i16x32, popcnt_i16x16, popcnt_i16x8, popcnt_u32x16, popcnt_u32x8, popcnt_u32x4, popcnt_i32x16, popcnt_i32x8, popcnt_i32x4, popcnt_u64x8, popcnt_u64x4, popcnt_u64x2, popcnt_i64x8, popcnt_i64x4, popcnt_i64x2));
 }
