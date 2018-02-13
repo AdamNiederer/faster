0cac6f73c2599abd27791487666f19466eda1dc2
Adam Niederer <adam.niederer@gmail.com>
2018-02-12 22:28:06 -0500

    Fix typo & remove unused code

--- src/intrin/popcnt.rs
+++ src/intrin/popcnt.rs
@@ -51,7 +51,7 @@ fn popcnt256(v: u8x32) -> usize {
 
 #[inline(always)]
 #[cfg(not(target_feature = "avx2"))]
-fn popcnt256(v: u8x64) -> usize {
+fn popcnt256(v: u8x32) -> usize {
     v.be_u64s().scalar_reduce(0, |acc, s| acc + (s.count_ones() as usize))
 }
 
@@ -74,15 +74,14 @@ macro_rules! impl_popcnt {
     }
 }
 
-impl_popcnt!(u8x64, popcnt512, u8x32, popcnt256, u8x16, popcnt128,
-             i8x64, popcnt512, i8x32, popcnt256, i8x16, popcnt128,
-             u16x32, popcnt512, u16x16, popcnt256, u16x8, popcnt128,
-             i16x32, popcnt512, i16x16, popcnt256, i16x8, popcnt128,
-             u32x16, popcnt512, u32x8, popcnt256, u32x4, popcnt128,
-             i32x16, popcnt512, i32x8, popcnt256, i32x4, popcnt128,
-             u64x8, popcnt512, u64x4, popcnt256, u64x2, popcnt128,
-             i64x8, popcnt512, i64x4, popcnt256, i64x2, popcnt128);
-
+impl_popcnt!(u8x64, popcnt512, u8x32, popcnt256, u8x16, popcnt128);
+impl_popcnt!(i8x64, popcnt512, i8x32, popcnt256, i8x16, popcnt128);
+impl_popcnt!(u16x32, popcnt512, u16x16, popcnt256, u16x8, popcnt128);
+impl_popcnt!(i16x32, popcnt512, i16x16, popcnt256, i16x8, popcnt128);
+impl_popcnt!(u32x16, popcnt512, u32x8, popcnt256, u32x4, popcnt128);
+impl_popcnt!(i32x16, popcnt512, i32x8, popcnt256, i32x4, popcnt128);
+impl_popcnt!(u64x8, popcnt512, u64x4, popcnt256, u64x2, popcnt128);
+impl_popcnt!(i64x8, popcnt512, i64x4, popcnt256, i64x2, popcnt128);
 
 #[cfg(test)]
 mod tests {
@@ -101,10 +100,6 @@ mod tests {
             )*
         )
     }
-
-    // test_popcnt!((u8, i8, u16, i16, u32, i32, u64, i64),
-    //              (u8x32, i8x32, u16x16, i16x16, u32x8, i32x8, u64x4, i64x4),
-    //              (popcnt_u8x32, popcnt_i8x32, popcnt_u16x16, popcnt_i16x16, popcnt_u32x8, popcnt_i32x8, popcnt_u64x4, popcnt_i64x4));
 
     test_popcnt!((u8, u8, u8, i8, i8, i8, u16, u16, u16, i16, i16, i16, u32, u32, u32, i32, i32, i32, u64, u64, u64, i64, i64, i64),
                  (u8x64, u8x32, u8x16, i8x64, i8x32, i8x16, u16x32, u16x16, u16x8, i16x32, i16x16, i16x8, u32x16, u32x8, u32x4, i32x16, i32x8, i32x4, u64x8, u64x4, u64x2, i64x8, i64x4, i64x2),
