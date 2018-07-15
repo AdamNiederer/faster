f6ff4923c3fc34c913977a0d47069d1b026e960f
Ralf Biedert <rb@xr.io>
2018-07-15 21:27:14 +0200

    Enabled actual detection.

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -1,13 +1,14 @@
 
-//#[cfg(target_feature = "sse")] 
-//pub mod x86;
+//#[cfg(target_feature = "sse")]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub mod x86;
 
-//#[cfg(not(target_feature = "sse"))] 
+#[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))] 
 pub mod unknown;
 
 
-//#[cfg(target_feature = "sse")] 
-//pub use self::x86 as current;
-//
-//#[cfg(not(target_feature = "sse"))]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub use self::x86 as current;
+
+#[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
 pub use self::unknown as current;
