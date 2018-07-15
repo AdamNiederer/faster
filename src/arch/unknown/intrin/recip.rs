0449ce06169083157cdc2a0d23bd172155125f4a
Ralf Biedert <rb@xr.io>
2018-07-15 16:57:56 +0200

    Now all tests work for `unknown`. Cleaned up some imports.

---
+++ src/arch/unknown/intrin/recip.rs
@@ -0,0 +1,35 @@
+// This file is part of faster, the SIMD library for humans.
+// Copyright 2017 Adam Niederer <adam.niederer@gmail.com>
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use crate::arch::current::vecs::*;
+use crate::vecs::*;
+use crate::intrin::recip::Recip;
+
+
+rust_fallback_impl! {
+    impl Recip for f32x4 where "__undefined" {
+        recip => __undefined(), [0, 1, 2, 3];
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::*;
+    use std::f32::INFINITY;
+
+    #[test]
+    fn recip_f32s() {
+        let mut i = -1024.0;
+        while i < 1024.0 {
+            // This test has some pretty significant float error if done on x86
+            let ans = f32s(i).recip().extract(0);
+            let real = f32s(1.0 / i).extract(0);
+            assert!((real == INFINITY && ans == INFINITY) || (ans - real).abs() < 0.0005);
+            i += 1.0
+        }
+    }
+}
