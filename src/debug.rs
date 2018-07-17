15a9bbd19e2beb192f28bfea64511ff8b9826bea
Ralf Biedert <rb@xr.io>
2018-07-17 13:25:28 +0200

    Performance inspection available with `--feature trace`.

--- src/debug.rs
+++ src/debug.rs
@@ -1,16 +1,38 @@
+#![allow(unused_macros, dead_code)]
+
 use std::collections::HashSet;
 use std::cell::RefCell;
 
 thread_local! {
-    // Not perfect, but better than a `global` hack to prevent multiple prints
-    // of the same warning.
+    // Not perfect as it might print multiple times (once per thread),
+    // but better than a `global` hack to prevent multiple prints of the
+    // same warning.
     pub(crate) static OUTPUT_GUARD: RefCell<HashSet<String>> = RefCell::new(HashSet::new());
+}
+
+
+macro_rules! debug_append_log {
+    ($str:expr) => {
+        use std::io::Write;
+
+        // Allows the user to configure debug file path at compile time,
+        // e.g., when building for embedded / Android.
+        let file_name = option_env!("FASTER_DEBUG_FILE").unwrap_or("faster-debug.log");
+
+        std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(file_name).and_then(|mut file| {
+                writeln!(file, "{}", $str)
+        }).ok(); // `ok` suppresses warning about unused results, about which we don't care.
+    }
 }
 
 
 /// Prints the given string once (for the current thread).
 /// Useful for not spamming the console.
-macro_rules! debug_print_once {
+macro_rules! debug_output_once {
     ($str:expr) => {
         let output = $str;
 
@@ -21,6 +43,8 @@ macro_rules! debug_print_once {
                 return;
             }
 
+            // Also print to file (if enabled).
+            debug_append_log!(output);
             println!("{}", output);
 
             output_guard.insert(output);
@@ -29,15 +53,29 @@ macro_rules! debug_print_once {
 }
 
 
+/// Signal a software fallback is executed.
+#[cfg(feature="trace")]
 macro_rules! fallback {
     () => {
-        debug_print_once!(format!("⛔ faster is using SOFTWARE emulation here ({}:{}).", file!(), line!()));
+        debug_output_once!(format!("⛔ faster is using SOFTWARE emulation here ({}:{}).", file!(), line!()));
     }
 }
 
-
+/// Signal an optimized SIMD intrinsic is executed.
+#[cfg(feature="trace")]
 macro_rules! optimized {
     () => {
-        debug_print_once!(format!("🚄 faster is using HARDWARE acceleration here ({}:{}).", file!(), line!()));
+        debug_output_once!(format!("🚄 faster is using HARDWARE acceleration here ({}:{}).", file!(), line!()));
     }
 }
+
+#[cfg(not(feature="trace"))]
+macro_rules! fallback {
+    () => { }
+}
+
+#[cfg(not(feature="trace"))]
+macro_rules! optimized {
+    () => { }
+}
+
